82a26db644e66266879e4a889369792443c3641f
Ricardo Cataldi <rcataldi@microsoft.com>
2024-12-09 09:50:22 -0300

    [FIX] Fixing project structure
    Fixed project structure to look more similar to engine

--- src/frontend/utils/api.ts
+++ src/frontend/utils/api.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 export const BACK_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost'
 
 const api = axios.create({
-  baseURL: process.env.WEB_APP_BASE_URL || "http://localhost:8081"
+  baseURL: process.env.WEB_APP_BASE_URL || "http://localhost:8084"
 });
 
 export const webApp = axios.create({
-  baseURL: process.env.WEB_APP_BASE_URL || "http://localhost:8000"
+  baseURL: process.env.WEB_APP_BASE_URL || "http://localhost:8084"
 });
 
 export const webApi = axios.create({
